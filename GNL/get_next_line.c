fee5457477202a146068d9464c062bba20e76990
Emeric Krause <ekrause@k0r1p3.42mulhouse.fr>
2023-12-14 11:51:28 +0100

    finished

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -1,123 +1,57 @@
-#include "get_next_line.h"
-#include <stdio.h> // A ENLEVER
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekrause <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 11:28:13 by ekrause           #+#    #+#             */
+/*   Updated: 2023/12/14 11:28:31 by ekrause          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char *get_next_line(int fd)
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
 {
 	static char	*stash = NULL;
 	char		*line;
+	int			readed;
 
+	readed = 1;
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) == -1)
 		return (NULL);
 	line = NULL;
-	read_and_add(fd, &line, &stash);
-	if (line[0] == '\0')
-	{
-		return (NULL);
-	}
+	read_and_add(fd, &line, &stash, &readed);
 	return (line);
 }
 
-void	read_and_add(int fd, char **line, char **stash)
+void	read_and_add(int fd, char **line, char **stash, int *readed)
 {
-	int		readed;
 	char	*buf;
 
-	readed = 1;
-	while (readed > 0 && !found_newline(*stash))
+	while (*readed > 0 && !found_newline(*stash))
 	{
 		buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 		if (!buf)
 			return ;
-		readed = read(fd, buf, BUFFER_SIZE);
-		if (readed == -1)
+		*readed = (int)read(fd, buf, BUFFER_SIZE);
+		if ((*stash == NULL && *readed == 0) || *readed == -1)
 		{
 			free (buf);
 			return ;
 		}
-		buf[readed] = '\0';
+		buf[*readed] = '\0';
 		(*stash) = ft_strjoin(*stash, buf);
 		free (buf);
 	}
+	if (*stash[0] == '\0')
+	{
+		free_stash(*stash);
+		return ;
+	}
 	extract_line(*stash, line);
 	clear_stash(stash);
-}
-
-int	found_newline(char *stash)
-{
-	int i;
-
-	if (!stash)
-		return (0);
-	i = 0;
-	while (stash[i])
-	{
-		if (stash[i] == '\n')
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char *ptr;
-	char *current_ptr;
-	char *previous;
-
-	if (!s1 && !s2)
-		return (NULL);
-	else if (!s2)
-		return (ft_strdup(s1));
-	else if (!s1)
-		return (ft_strdup(s2));
-	ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!ptr)
-		return (NULL);
-	current_ptr = ptr;
-	previous = s1;
-	while (*s1)
-		*ptr++ = *s1++;
-	while (*s2)
-		*ptr++ = *s2++;
-	*ptr = '\0';
-	free_stash(previous);
-	return (current_ptr);
-}
-
-void free_stash(char *stash)
-{
-    if (stash)
-	{
-        free(stash);
-        stash = NULL;
-    }
-}
-
-int	ft_strlen(char *str)
-{
-	int i;
-
-	if (!str)
-		return (0);
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char *ft_strdup(char *str)
-{
-	char *ptr;
-	char *current_ptr;
-
-	ptr = malloc(sizeof(char) * (ft_strlen(str) + 1));
-	if (!ptr)
-		return (NULL);
-	current_ptr = ptr;
-	while (*str)
-		*ptr++ = *str++;
-	*ptr = '\0';
-	return (current_ptr);
 }
 
 void	extract_line(char *stash, char **line)
